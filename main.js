c1fbedd9d7216a24e892c20578312844141c8be1
ken <76490082+SuenKen@users.noreply.github.com>
2021-03-18 14:06:15 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let swRegistration;
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+navigator.serviceWorker
+    .register('/js/sw.js')
+    .then(swReg => {
+      console.log('Service Worker is registered', swReg);
+      swRegistration = swReg;
+    })
+    .catch(err => console.log('Service Worker register error.'));
+} else {
+  console.log('Push is not supported');
+}
